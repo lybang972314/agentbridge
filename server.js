0019174c6f28e31b4dfe1e0f34c7aeb0efe14b3d
我兜有糖 <lybang972314@gmail.com>
2026-06-18 23:13:51 +0800

    test: plain JS server - zero npm deps at runtime

---
+++ server.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify({status:'ok', version:'test', path: req.url}));
+});
+server.listen(3100, () => console.log('Test server on 3100'));
